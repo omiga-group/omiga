6482e80899ae0091203df04cde0933935cb1a9f6
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-08-07 22:12:20 +1200

    Added migration cli

---
+++ src/order/shared/repositories/cli/commands/root.go
@@ -0,0 +1,31 @@
+package commands
+
+import (
+	"github.com/micro-business/go-core/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+// RootCommand returns root CLI application command interface
+func Root() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use: "order-database-migration",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			printHeader()
+		},
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	// Register all commands
+	cmd.AddCommand(
+		versionCommand(),
+		migrateCommand(),
+	)
+
+	return cmd
+}
+
+func printHeader() {
+	util.PrintInfo("Order Database Migration Tool")
+}
