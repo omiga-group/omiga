3d9c87d4a2b78195f79c5644154ece945a229093
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-08-14 21:22:09 +1200

    Added test omiga processor

---
+++ src/exchange/omiga-processor/commands/start.go
@@ -0,0 +1,91 @@
+package commands
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/mitchellh/mapstructure"
+	syntheticorderv1 "github.com/omiga-group/omiga/src/shared/clients/events/omiga/synthetic-order/v1"
+	"github.com/omiga-group/omiga/src/shared/enterprise/configuration"
+	"github.com/omiga-group/omiga/src/shared/enterprise/messaging/pulsar"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+func startCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start omiga-processor",
+		Long:  "Start omiga-processor",
+		Run: func(cmd *cobra.Command, args []string) {
+			logger, err := zap.NewDevelopment()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			sugarLogger := logger.Sugar()
+
+			viper, err := configuration.SetupConfigReader(".")
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			var pulsarSettings pulsar.PulsarSettings
+			if err := mapstructure.Decode(viper.Get(pulsar.ConfigKey), &pulsarSettings); err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			ctx, cancelFunc := context.WithCancel(context.Background())
+
+			sigc := make(chan os.Signal, 1)
+			signal.Notify(sigc,
+				syscall.SIGHUP,
+				syscall.SIGINT,
+				syscall.SIGTERM,
+				syscall.SIGQUIT)
+			go func() {
+				<-sigc
+				cancelFunc()
+			}()
+
+			messageConsumer, err := NewMessageConsumer(
+				sugarLogger,
+				pulsarSettings,
+				syntheticorderv1.TopicName)
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			defer messageConsumer.Close(ctx)
+
+			syntheticOrderConsumer, err := NewSyntheticOrderConsumer(
+				sugarLogger,
+				messageConsumer)
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			err = syntheticOrderConsumer.StartAsync(ctx)
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			for {
+				if ctx.Err() == context.Canceled {
+					break
+				}
+
+				select {
+				case <-ctx.Done():
+				case <-time.After(time.Second):
+				}
+			}
+		},
+	}
+
+	return cmd
+}
