917f6c1434a100258e05c5348106e2a34593828b
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-08-14 21:11:12 +1200

    Added synthetic order subscriber to the binance processor

--- src/exchange/binance-processor/commands/start.go
+++ src/exchange/binance-processor/commands/start.go
@@ -1,9 +1,17 @@
 package commands
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/mitchellh/mapstructure"
+	syntheticorderv1 "github.com/omiga-group/omiga/src/shared/clients/events/omiga/synthetic-order/v1"
+	"github.com/omiga-group/omiga/src/shared/enterprise/configuration"
+	"github.com/omiga-group/omiga/src/shared/enterprise/messaging/pulsar"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
@@ -19,9 +27,63 @@ func startCommand() *cobra.Command {
 				log.Fatal(err)
 			}
 
-			_ = logger.Sugar()
+			sugarLogger := logger.Sugar()
 
-			time.Sleep(1 * time.Minute)
+			viper, err := configuration.SetupConfigReader(".")
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			var pulsarSettings pulsar.PulsarSettings
+			if err := mapstructure.Decode(viper.Get(pulsar.ConfigKey), &pulsarSettings); err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			ctx, cancelFunc := context.WithCancel(context.Background())
+
+			sigc := make(chan os.Signal, 1)
+			signal.Notify(sigc,
+				syscall.SIGHUP,
+				syscall.SIGINT,
+				syscall.SIGTERM,
+				syscall.SIGQUIT)
+			go func() {
+				<-sigc
+				cancelFunc()
+			}()
+
+			messageConsumer, err := NewMessageConsumer(
+				sugarLogger,
+				pulsarSettings,
+				syntheticorderv1.TopicName)
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			defer messageConsumer.Close(ctx)
+
+			syntheticOrderConsumer, err := NewSyntheticOrderConsumer(
+				sugarLogger,
+				messageConsumer)
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			err = syntheticOrderConsumer.StartAsync(ctx)
+			if err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			for {
+				if ctx.Err() == context.Canceled {
+					break
+				}
+
+				select {
+				case <-ctx.Done():
+				case <-time.After(time.Second):
+				}
+			}
 		},
 	}
 
---
+++ src/exchange/binance-processor/commands/wire.go
@@ -0,0 +1,40 @@
+// Copyright 2018 The Wire Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+package commands
+
+import (
+	"github.com/google/wire"
+	"github.com/omiga-group/omiga/src/exchange/binance-processor/subscribers"
+	syntheticorderv1 "github.com/omiga-group/omiga/src/shared/clients/events/omiga/synthetic-order/v1"
+	"github.com/omiga-group/omiga/src/shared/enterprise/messaging"
+	"github.com/omiga-group/omiga/src/shared/enterprise/messaging/pulsar"
+	"go.uber.org/zap"
+)
+
+func NewMessageConsumer(logger *zap.SugaredLogger, pulsarSettings pulsar.PulsarSettings, topic string) (messaging.MessageConsumer, error) {
+	wire.Build(pulsar.NewPulsarMessageConsumer)
+
+	return nil, nil
+}
+
+func NewSyntheticOrderConsumer(logger *zap.SugaredLogger, messageConsumer messaging.MessageConsumer) (syntheticorderv1.Consumer, error) {
+	wire.Build(syntheticorderv1.NewConsumer, subscribers.NewSyntheticOrderSubscriber)
+
+	return nil, nil
+}
