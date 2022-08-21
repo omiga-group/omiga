af597f3f0fbda820137c9f4aa8fa1e1a9cf32868
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-08-22 00:45:04 +1200

    Added Graphql gateway - federation (#15)

---
+++ src/gateway/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [{ name: "orders", url: process.env.ORDER_GRAPHQL_API }],
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+server
+  .listen({
+    port: process.env.PORT,
+    host: "0.0.0.0"
+  })
+  .then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
