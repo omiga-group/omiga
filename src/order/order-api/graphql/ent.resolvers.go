a5828dbefe2a7cc9ff8d51594185a5c53aab84e3
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-08-21 23:31:48 +1200

    Refactoring

--- src/order/order-api/graphql/ent.resolvers.go
+++ src/order/order-api/graphql/ent.resolvers.go
@@ -7,32 +7,33 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/omiga-group/omiga/src/order/shared"
+	"github.com/omiga-group/omiga/src/order/order-api/graphql/generated"
+	"github.com/omiga-group/omiga/src/order/order-api/graphql/models"
 	"github.com/omiga-group/omiga/src/order/shared/repositories"
 )
 
 // Status is the resolver for the status field.
-func (r *outboxWhereInputResolver) Status(ctx context.Context, obj *repositories.OutboxWhereInput, data *shared.OutboxStatus) error {
+func (r *outboxWhereInputResolver) Status(ctx context.Context, obj *repositories.OutboxWhereInput, data *models.OutboxStatus) error {
 	panic(fmt.Errorf("not implemented"))
 }
 
 // StatusNeq is the resolver for the statusNEQ field.
-func (r *outboxWhereInputResolver) StatusNeq(ctx context.Context, obj *repositories.OutboxWhereInput, data *shared.OutboxStatus) error {
+func (r *outboxWhereInputResolver) StatusNeq(ctx context.Context, obj *repositories.OutboxWhereInput, data *models.OutboxStatus) error {
 	panic(fmt.Errorf("not implemented"))
 }
 
 // StatusIn is the resolver for the statusIn field.
-func (r *outboxWhereInputResolver) StatusIn(ctx context.Context, obj *repositories.OutboxWhereInput, data []shared.OutboxStatus) error {
+func (r *outboxWhereInputResolver) StatusIn(ctx context.Context, obj *repositories.OutboxWhereInput, data []models.OutboxStatus) error {
 	panic(fmt.Errorf("not implemented"))
 }
 
 // StatusNotIn is the resolver for the statusNotIn field.
-func (r *outboxWhereInputResolver) StatusNotIn(ctx context.Context, obj *repositories.OutboxWhereInput, data []shared.OutboxStatus) error {
+func (r *outboxWhereInputResolver) StatusNotIn(ctx context.Context, obj *repositories.OutboxWhereInput, data []models.OutboxStatus) error {
 	panic(fmt.Errorf("not implemented"))
 }
 
-// OutboxWhereInput returns shared.OutboxWhereInputResolver implementation.
-func (r *Resolver) OutboxWhereInput() shared.OutboxWhereInputResolver {
+// OutboxWhereInput returns generated.OutboxWhereInputResolver implementation.
+func (r *Resolver) OutboxWhereInput() generated.OutboxWhereInputResolver {
 	return &outboxWhereInputResolver{r}
 }
 
