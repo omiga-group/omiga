6ab324118031110d83e9f72979ef2ab904c88ac5
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-08-26 23:15:07 +1200

    Added coingeko processor (#21)

---
+++ src/exchange/coingeko/commands/start.go
@@ -0,0 +1,69 @@
+package commands
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/omiga-group/omiga/src/exchange/coingeko/configuration"
+	entconfiguration "github.com/omiga-group/omiga/src/shared/enterprise/configuration"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+)
+
+type startOptions struct {
+	name string
+}
+
+func startCommand() *cobra.Command {
+	opt := startOptions{}
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start coingeko",
+		Long:  "Start coingeko",
+		Run: func(cmd *cobra.Command, args []string) {
+			logger, err := zap.NewDevelopment()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			sugarLogger := logger.Sugar()
+
+			var config configuration.Config
+			if err := entconfiguration.LoadConfig("config.yaml", &config); err != nil {
+				sugarLogger.Fatal(err)
+			}
+
+			ctx, cancelFunc := context.WithCancel(context.Background())
+
+			sigc := make(chan os.Signal, 1)
+			signal.Notify(sigc,
+				syscall.SIGHUP,
+				syscall.SIGINT,
+				syscall.SIGTERM,
+				syscall.SIGQUIT)
+			go func() {
+				<-sigc
+				cancelFunc()
+			}()
+
+			for {
+				if ctx.Err() == context.Canceled {
+					break
+				}
+
+				select {
+				case <-ctx.Done():
+				case <-time.After(time.Second):
+				}
+			}
+		},
+	}
+
+	cmd.Flags().StringVar(&opt.name, "name", "", "The coingeko instance name")
+
+	return cmd
+}
