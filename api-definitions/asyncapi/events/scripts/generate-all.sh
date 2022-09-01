508936352d0a9989e546030943e69c89fc68fa4b
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-09-01 13:15:52 +1200

    Added websocket template

--- api-definitions/asyncapi/events/scripts/generate-all.sh
+++ api-definitions/asyncapi/events/scripts/generate-all.sh
@@ -6,15 +6,15 @@ set -x
 cd "$(dirname "${0}")/.."
 
 cleanup() {
-   docker rm extract-asyncapi-generator || true
+   docker rm extract-asyncapi-events-generator || true
 }
 trap cleanup EXIT
 
 # Delete previously generated golang files
-find ../../src/shared/events -name "*_gen.go" -type f -delete || true
+find "../../../src/shared/events" -name "*_gen.go" -type f -delete || true
 
 # AsyncApi
-docker build --progress=plain -f Dockerfile -t asyncapi-generator ../../../
-docker create --name extract-asyncapi-generator asyncapi-generator
+docker build --progress=plain -f Dockerfile -t asyncapi-events-generator ../../../
+docker create --name extract-asyncapi-events-generator asyncapi-events-generator
 mkdir -p "../../../src/shared/clients/events"
-docker cp extract-asyncapi-generator:/generated-src/. "../../../src/shared/clients/events/"
+docker cp extract-asyncapi-events-generator:/generated-src/. "../../../src/shared/clients/events/"
