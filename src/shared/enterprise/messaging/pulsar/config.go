2eef1c25b44c9f60b8f78470752819ea03b5b710
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-09-13 20:03:46 +1200

    Added json schema generation

--- src/shared/enterprise/messaging/pulsar/config.go
+++ src/shared/enterprise/messaging/pulsar/config.go
@@ -1,6 +1,7 @@
 package pulsar
 
 type PulsarConfig struct {
+	HttpUrl                    string                           `yaml:"httpUrl" env:"OMIGA_PULSAR_HTTPURL"`
 	Url                        string                           `yaml:"url" env:"OMIGA_PULSAR_URL"`
 	ProducerName               string                           `yaml:"producerName" env:"OMIGA_PULSAR_PRODUCERNAME"`
 	SubscriptionName           string                           `yaml:"subscriptionName" env:"OMIGA_PULSAR_SUBSCRIPTIONNAME"`
