b3b0ff48bfb0ad0901eef7186d3d40f716164369
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2022-10-30 00:59:23 +1300

    Fixed schedule

--- src/venue/coingecko-processor/subscribers/coingecko_exchange_subscriber.go
+++ src/venue/coingecko-processor/subscribers/coingecko_exchange_subscriber.go
@@ -49,8 +49,8 @@ func NewCoingeckoExchangeSubscriber(
 		venueRepository: venueRepository,
 	}
 
-	// Run at every second from 0 through 59.
-	if _, err := cronService.GetCron().AddJob("0/1 * * * * *", instance); err != nil {
+	// Run at every 5th minute from 0 through 59..
+	if _, err := cronService.GetCron().AddJob("* 0/5 * * * *", instance); err != nil {
 		return nil, err
 	}
 
