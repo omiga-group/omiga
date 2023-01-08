cd241ebbe61c050069aa938d23297b95db4d0b31
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2023-01-08 20:50:18 +1300

    Added post migration script content to the initial script

--- src/venue/shared/migrations/20230108072510_initial.sql
+++ src/venue/shared/migrations/20230108072510_initial.sql
@@ -110,3 +110,66 @@ CREATE INDEX "tradingpair_counter_quantity_min_precision" ON "trading_pairs" ("c
 CREATE INDEX "tradingpair_counter_quantity_max_precision" ON "trading_pairs" ("counter_quantity_max_precision");
 -- create "market_trading_pair" table
 CREATE TABLE "market_trading_pair" ("market_id" bigint NOT NULL, "trading_pair_id" bigint NOT NULL, PRIMARY KEY ("market_id", "trading_pair_id"), CONSTRAINT "market_trading_pair_market_id" FOREIGN KEY ("market_id") REFERENCES "markets" ("id") ON DELETE CASCADE, CONSTRAINT "market_trading_pair_trading_pair_id" FOREIGN KEY ("trading_pair_id") REFERENCES "trading_pairs" ("id") ON DELETE CASCADE);
+
+
+-- Custom scripts
+DO
+$do$
+BEGIN
+	IF NOT EXISTS (SELECT 1
+       FROM pg_catalog.pg_constraint con
+            INNER JOIN pg_catalog.pg_class rel
+                       ON rel.oid = con.conrelid
+            INNER JOIN pg_catalog.pg_namespace nsp
+                       ON nsp.oid = connamespace
+       WHERE nsp.nspname = 'public'
+             AND rel.relname = 'currencies'
+             AND con.conname = 'currencies_symbol_key') THEN
+		ALTER TABLE public.currencies 
+		ADD CONSTRAINT currencies_symbol_key
+		UNIQUE (symbol); 
+	END IF;
+
+	IF NOT EXISTS (SELECT 1
+       FROM pg_catalog.pg_constraint con
+            INNER JOIN pg_catalog.pg_class rel
+                       ON rel.oid = con.conrelid
+            INNER JOIN pg_catalog.pg_namespace nsp
+                       ON nsp.oid = connamespace
+       WHERE nsp.nspname = 'public'
+             AND rel.relname = 'venues'
+             AND con.conname = 'venues_venue_id_key') THEN
+		ALTER TABLE public.venues 
+		ADD CONSTRAINT venues_venue_id_key
+		UNIQUE (venue_id); 
+	END IF;
+
+	IF NOT EXISTS (SELECT 1
+       FROM pg_catalog.pg_constraint con
+            INNER JOIN pg_catalog.pg_class rel
+                       ON rel.oid = con.conrelid
+            INNER JOIN pg_catalog.pg_namespace nsp
+                       ON nsp.oid = connamespace
+       WHERE nsp.nspname = 'public'
+             AND rel.relname = 'tickers'
+             AND con.conname = 'tickers_base_counter_venue_ticker_key') THEN
+		ALTER TABLE public.tickers 
+		ADD CONSTRAINT tickers_base_counter_venue_ticker_key
+		UNIQUE (base, counter, venue_ticker); 
+	END IF;
+
+	IF NOT EXISTS (SELECT 1
+       FROM pg_catalog.pg_constraint con
+            INNER JOIN pg_catalog.pg_class rel
+                       ON rel.oid = con.conrelid
+            INNER JOIN pg_catalog.pg_namespace nsp
+                       ON nsp.oid = connamespace
+       WHERE nsp.nspname = 'public'
+             AND rel.relname = 'trading_pairs'
+             AND con.conname = 'trading_pairs_symbol_venue_trading_pair_key') THEN
+		ALTER TABLE public.trading_pairs 
+		ADD CONSTRAINT trading_pairs_symbol_venue_trading_pair_key
+		UNIQUE (symbol, venue_trading_pair); 
+	END IF;
+END
+$do$
